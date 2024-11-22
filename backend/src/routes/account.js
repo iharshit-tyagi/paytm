c3e6c48594a53c073fea510e75becdd05b091970
Harshit <harshitt52@gmail.com>
2024-11-22 16:41:56 +0530

    Route to Update Balance

--- backend/src/routes/account.js
+++ backend/src/routes/account.js
@@ -21,9 +21,54 @@ else{
     })
 }
 }
+const findToAccount=async(req,res,next)=>{
+    const toUser = await User.findOne({username:req?.body?.to});
+    if(!toUser){
+        res.status(400).json({
+            message: "Invalid account"
+        })
+    }
+req.toUser=toUser;
+const fromUser = await User.findOne({username:req?.userID});
+req.fromUser=fromUser
+next();
+}
+const doTransaction=async(req,res,next)=>{
+    const fromAccount= await Account.findOne({userID:req?.fromUser?._id});
+    const toAccount= await Account.findOne({userID:req?.toUser?._id});
+    const fromBalance= fromAccount?.balance;
+    const toBalance= toAccount?.balance;
+    if (fromAccount?.balance<req?.body?.amount){
+        res.status(400).json({
+            message:'Insufficient Balance'
+        })
+        return;
+    }
+
+    try{
+     await    Account.updateOne({userID:fromAccount?.userID},{balance:(fromBalance-req?.body?.amount)})
+   await      Account.updateOne({userID:toAccount?.userID},{balance:(toBalance+req?.body?.amount)})
+//    await fromAccount.updateOne({balance:(fromBalance-req?.body?.amount)})
+//     await fromAccount.updateOne({balance:(toBalance+req?.body?.amount)})
+      next();
+    }catch(err){
+        await fromAccount.updateOne({balance:fromBalance})
+        await fromAccount.updateOne({balance:toBalance})
+        res.status(401).json({
+            message:'Transaction Failed!'
+        })
+        return;
+    }
+}
 accountRoute.get('/balance',authMiddleware,getBalanceFromDB,(req,res)=>{
     res.status(200).json({
         balance: req?.balance
+    })
+})
+
+accountRoute.post('/transfer',authMiddleware,findToAccount,doTransaction,(req,res)=>{
+    res.status(200).json({
+        message: "Transfer successful"
     })
 })
 export default accountRoute;
